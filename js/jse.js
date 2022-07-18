be95e5b268454af84b200ae76f48d1ad02a79e09
fabiangomezfranco27 <102840961+fabiangomezfranco27@users.noreply.github.com>
2022-07-18 14:35:45 -0500

    Agregado de evaluación y actividades

--- js/jse.js
+++ js/jse.js
@@ -9,10 +9,67 @@ function calcular(){
     var h= document.getElementById('h').value;
     var i= document.getElementById('i').value;
     var j= document.getElementById('j').value;
+    var acertadas = 0;
 
 switch(a){
-    case "v": r='Correcto'; break;
+    case "v": r='Correcto'; acertadas++; break;
     default: r='Incorrecto'; break;
 }
-document.getElementById("resultado").innerHTML=r;
+document.getElementById("ra").innerHTML=r;
+
+switch(b){
+    case "v": r='Correcto'; acertadas++; break;
+    default: r='Incorrecto'; break;
+}
+document.getElementById("rb").innerHTML=r;
+
+switch(c){
+    case "v": r='Correcto'; acertadas++; break;
+    default: r='Incorrecto'; break;
+}
+document.getElementById("rc").innerHTML=r;
+
+switch(d){
+    case "v": r='Correcto'; acertadas++; break;
+    default: r='Incorrecto'; break;
+}
+document.getElementById("rd").innerHTML=r;
+
+switch(e){
+    case "v": r='Correcto'; acertadas++; break;
+    default: r='Incorrecto'; break;
+}
+document.getElementById("re").innerHTML=r;
+
+switch(f){
+    case "v": r='Correcto'; acertadas++; break;
+    default: r='Incorrecto'; break;
+}
+document.getElementById("rf").innerHTML=r;
+
+switch(g){
+    case "v": r='Correcto'; acertadas++; break;
+    default: r='Incorrecto'; break;
+}
+document.getElementById("rg").innerHTML=r;
+
+switch(h){
+    case "v": r='Correcto'; acertadas++; break;
+    default: r='Incorrecto'; break;
+}
+document.getElementById("rh").innerHTML=r;
+
+switch(i){
+    case "v": r='Correcto'; acertadas++; break;
+    default: r='Incorrecto'; break;
+}
+document.getElementById("ri").innerHTML=r;
+
+switch(j){
+    case "v": r='Correcto'; acertadas++; break;
+    default: r='Incorrecto'; break;
+}
+document.getElementById("rj").innerHTML=r;
+
+document.getElementById("nota").innerHTML=(acertadas/2);
 }
